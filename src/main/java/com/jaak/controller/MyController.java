3d5c7738eccb70be49a19fab13d4a81d07206edf
Akshay <akshay.nanavare@clairvoyantsoft.com>
2022-04-08 18:11:31 +0530

    Docker project commit done!

---
+++ src/main/java/com/jaak/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.jaak.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/message")
+    public String getMessage(){
+        return "Welcome to Docker";
+    }
+}
